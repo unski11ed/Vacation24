4a1c64e0d50e400dd873bfef0bc0dc1f36b9a693
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-03-26 23:37:03 +0100

    Updated OwnerController

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -4,12 +4,10 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Vacation24.Models;
-using WebMatrix.WebData;
-using PagedList;
-using PagedList.Mvc;
 using Vacation24.Core;
 using Vacation24.Core.Payment;
 using Vacation24.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Vacation24.Controllers
 {
@@ -19,53 +17,66 @@ namespace Vacation24.Controllers
         // GET: /Owner/
         private const int MAX_OBJECTS_PER_PAGE = 4;
 
-        private DefaultContext _dbContext = new DefaultContext();
-        private IPaymentServices _paymentServices;
+        private readonly IPaymentServices paymentServices;
+        private readonly DefaultContext dbContext;
+        private readonly CurrentUserProvider currentUserProvider;
 
-        public OwnerController(IPaymentServices paymentServices)
+        public OwnerController(
+            IPaymentServices paymentServices,
+            DefaultContext dbContext,
+            CurrentUserProvider currentUserProvider
+        )
         {
-            _paymentServices = paymentServices;
+            this.paymentServices = paymentServices;
+            this.dbContext = dbContext;
+            this.currentUserProvider = currentUserProvider;
         }
 
         [HttpGet]
         [Authorize(Roles = "owner, admin")]
         public ActionResult Index()
         {
-            var totalUserObjects = _dbContext.Places.Where(p => p.OwnerId == WebSecurity.CurrentUserId)
-                                                    .Count();
+            var totalUserObjects = dbContext.Places
+                .Where(p => p.OwnerId == currentUserProvider.UserId)
+                .Count();
 
             ViewBag.TotalPages = Math.Ceiling((decimal)totalUserObjects / MAX_OBJECTS_PER_PAGE);
 
             ViewBag.TotalUniqueViews = totalUserObjects >= 1 ?
-                                       _dbContext.Places.Where(p => p.OwnerId == WebSecurity.CurrentUserId)
-                                                        .Select(p => p.UniqueViews).Sum()
-                                                        : 0;
+                dbContext.Places
+                    .Where(p => p.OwnerId == currentUserProvider.UserId)
+                    .Select(p => p.UniqueViews)
+                    .Sum() :
+                0;
 
             ViewBag.TotalViews = totalUserObjects >= 1 ?
-                                       _dbContext.Places.Where(p => p.OwnerId == WebSecurity.CurrentUserId)
-                                                        .Select(p => p.Views).Sum()
-                                                        : 0;
+                dbContext.Places
+                    .Where(p => p.OwnerId == currentUserProvider.UserId)
+                    .Select(p => p.Views)
+                    .Sum() :
+                0;
 
-            var subscription = _paymentServices.GetUserServices<SubscriptionService>(WebSecurity.CurrentUserId)
-                                               .FirstOrDefault();
+            var subscription = paymentServices
+                .GetUserServices<SubscriptionService>(currentUserProvider.UserId)
+                .FirstOrDefault();
 
             ViewBag.IsSubscribed = IsCurrentUserAdmin() || (subscription != null && subscription.IsActive);
-
 
             return View();
         }
 
         [HttpGet]
         [Authorize(Roles = "owner, admin")]
-        public ActionResult GetUserObjects(int page = 0, int userId = -1)
+        public ActionResult GetUserObjects(int page = 0, string userId = "")
         {
-            var uid = IsCurrentUserAdmin() && userId > 0 ? userId : WebSecurity.CurrentUserId;
+            var uId = IsCurrentUserAdmin() && userId != String.Empty ? userId : currentUserProvider.UserId;
 
-            var objects = _dbContext.Places.Where(p => p.OwnerId == uid)
-                                           .OrderByDescending(p => p.Created)
-                                           .Skip(MAX_OBJECTS_PER_PAGE * page)
-                                           .Take(MAX_OBJECTS_PER_PAGE)
-                                           .ToList();
+            var objects = dbContext.Places
+                .Where(p => p.OwnerId == uId)
+                .OrderByDescending(p => p.Created)
+                .Skip(MAX_OBJECTS_PER_PAGE * page)
+                .Take(MAX_OBJECTS_PER_PAGE)
+                .ToList();
 
             return View(objects);
         }
