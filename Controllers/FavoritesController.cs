08341e2be7eef461215b046ef582a122dac86499
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-03-26 23:06:51 +0100

    Updated FavoritesController

--- Controllers/FavoritesController.cs
+++ Controllers/FavoritesController.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Vacation24.Core;
 using Vacation24.Models;
-using Vacation24.Models.DTO;
-using WebMatrix.WebData;
 
 namespace Vacation24.Controllers
 {
     public class FavoritesController : CustomController
     {
         private const int MAX_FAVS = 20;
-        private DefaultContext _dbContext = new DefaultContext();
+        private readonly DefaultContext dbContext;
+        private readonly CurrentUserProvider currentUserProvider;
+
+        public FavoritesController(
+            DefaultContext dbContext,
+            CurrentUserProvider currentUserProvider
+        )
+        {
+            this.dbContext = dbContext;
+            this.currentUserProvider = currentUserProvider;
+        }
 
         #region Embeded
         public ActionResult ShowUserBox()
         {
-            var favs = _dbContext.Favorites.Where(fav => fav.UserId == WebSecurity.CurrentUserId)
-                                           .OrderByDescending(fav => fav.Added)
-                                           .ToList();
+            var favs = dbContext.Favorites
+                .Where(fav => fav.UserId == currentUserProvider.UserId)
+                .OrderByDescending(fav => fav.Added)
+                .ToList();
 
             return View(favs);
         }
@@ -30,52 +40,64 @@ namespace Vacation24.Controllers
         [Authorize(Roles = "user, owner, admin")]
         public ActionResult Create(Favorite favorite) 
         {
-            favorite.UserId = WebSecurity.CurrentUserId;
+            favorite.UserId = currentUserProvider.UserId;
             favorite.Added = DateTime.Now;
 
-            //Sprawdź czy ilosc nie przekracza MAX_FAVS
-            var favoritesCount = _dbContext.Favorites.Where(fav => fav.UserId == WebSecurity.CurrentUserId).Count();
+            // Check if user hasn't passed MAX_FAVS
+            var favoritesCount = dbContext.Favorites
+                .Where(fav => fav.UserId == favorite.UserId)
+                .Count();
             if (favoritesCount >= MAX_FAVS)
             {
                 return Json(new ResultViewModel() {
                     Status = (int)ResultStatus.Info, 
-                    Message = string.Format("Można mieć maksymalnie {0} ulubionych obiektów.", MAX_FAVS)
+                    Message = string.Format("You can have total of {0} favorited places.", MAX_FAVS)
                 } );
             }
 
-            var favExists = _dbContext.Favorites.Where(fav => fav.UserId == WebSecurity.CurrentUserId &&
-                                                              fav.PlaceId == favorite.PlaceId).Count() > 0;
+            var favExists = dbContext.Favorites
+                .Where(fav => (
+                        fav.UserId == currentUserProvider.UserId &&
+                        fav.PlaceId == favorite.PlaceId
+                    )
+                )
+                .Count() > 0;
             if (favExists)
             {
                 return Json(new ResultViewModel()
                 {
                     Status = (int)ResultStatus.Info,
-                    Message = "Obiekt znajduje się już na liście ulubionych."
+                    Message = "This object is already in your favorites list."
                 });
             }
 
             try
             {
-                _dbContext.Favorites.Add(favorite);
-                _dbContext.SaveChanges();
-            }catch(Exception){
-                return Json(new ResultViewModel(){Status = (int)ResultStatus.Error, Message = "Nieudany zapis do bazy danych"});
+                dbContext.Favorites.Add(favorite);
+                dbContext.SaveChanges();
+            } catch(Exception) {
+                return Json(new ResultViewModel() { Status = (int)ResultStatus.Error, Message = "Failed to save to favorites" });
             }
             
-            return  Json(new ResultViewModel(){Status = (int)ResultStatus.Success});
+            return  Json(new ResultViewModel() { Status = (int)ResultStatus.Success });
         }
 
         [Authorize(Roles = "user, owner, admin")]
         public ActionResult Delete(RequestId requrestFavorite){
-            var favorite = _dbContext.Favorites.Where(fav => fav.Id == requrestFavorite.Id).FirstOrDefault();
+            var favorite = dbContext.Favorites
+                .Where(fav => fav.Id == requrestFavorite.Id)
+                .FirstOrDefault();
 
-            if (favorite == null || favorite.UserId != WebSecurity.CurrentUserId)
+            if (
+                favorite == null ||
+                favorite.UserId != currentUserProvider.UserId
+            )
             {
                 return Json(new ResultViewModel() { Status = (int)ResultStatus.Error, Message = "Nie znaleziono rekordu" });
             }
 
-            _dbContext.Favorites.Remove(favorite);
-            _dbContext.SaveChanges();
+            dbContext.Favorites.Remove(favorite);
+            dbContext.SaveChanges();
 
             return Json(new ResultViewModel() { Status = (int)ResultStatus.Success });
         }
--- Models/FavoritesModel.cs
+++ Models/FavoritesModel.cs
@@ -13,7 +13,7 @@ namespace Vacation24.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public int PlaceId { get; set; }
 
         public virtual Place Place { get; set; }
