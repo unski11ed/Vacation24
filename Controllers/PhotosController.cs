38b2a8079b92a603d2f21cb1f07d935e2a37141b
Maciej Kurbański <maciej.kurbanski@outlook.com>
2019-03-27 00:33:58 +0100

    Updated PhotosController

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -7,39 +7,56 @@ using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Vacation24.Core.Helpers;
 using Vacation24.Core.ExtensionMethods;
-using System.Data.Entity;
 using Vacation24.Models;
 using Vacation24.Core.Configuration.Images;
-using Vacation24.Models.DTO;
 using System.IO;
-using System.Drawing.Imaging;
 using Vacation24.Core;
-using WebMatrix.WebData;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
 
 namespace Vacation24.Controllers
 {
     public class PhotosController : CustomController
     {
-        private DefaultContext _dbContext = new DefaultContext();
+        private readonly DefaultContext dbContext;
+        private readonly CurrentUserProvider currentUserProvider;
+
+        public PhotosController(
+            DefaultContext dbContext,
+            CurrentUserProvider currentUserProvider
+        )
+        {
+            this.dbContext = dbContext;
+            this.currentUserProvider = currentUserProvider;
+        }
 
         public ActionResult GetThumbnails(int objectId)
         {
-            var imagesList = _dbContext.Photos.Where(photo => photo.PlaceId == objectId).ToList();
+            var imagesList = dbContext.Photos
+                .Where(photo => photo.PlaceId == objectId)
+                .ToList();
 
             return Json(imagesList);
         }
 
         public ActionResult Get(int photoId)
         {
-            var image = _dbContext.Photos.Where(photo => photo.Id == photoId).FirstOrDefault();
+            var image = dbContext.Photos
+                .Where(photo => photo.Id == photoId)
+                .FirstOrDefault();
 
             return Json(image);
         }
 
         [Authorize(Roles = "owner, admin")]
-        public ActionResult Add(int placeId, bool isMain)
+        public async Task<IActionResult> Add(int placeId, bool isMain)
         {
-            if (!IsCurrentUserAdmin() && !_dbContext.Places.Any(p => p.Id == placeId && p.OwnerId == WebSecurity.CurrentUserId))
+            if (
+                !IsCurrentUserAdmin() &&
+                !dbContext.Places.Any(
+                    p => p.Id == placeId && p.OwnerId == currentUserProvider.UserId
+                )
+            )
             {
                 return Json(new ResultViewModel()
                 {
@@ -49,66 +66,92 @@ namespace Vacation24.Controllers
             }
 
 
-            for (int i = 0; i < Request.Files.Count; i++)
+            for (int i = 0; i < Request.Form.Files.Count; i++)
             {
-                var file = Request.Files[i];
-                if(file.ContentType == "image/jpeg" ||
-                   file.ContentType == "image/png")
+                var file = Request.Form.Files[i];
+                if (
+                    file.ContentType == "image/jpeg" ||
+                    file.ContentType == "image/png"
+                )
                 {
-                    var img = Image.FromStream(file.InputStream);
+                    using(var fileStream = file.OpenReadStream()) {
+                        //Generate thumbnails
+                        var thumbnails = Thumbnail.Sizes
+                            .Select(
+                                size => new
+                                    {
+                                        stream = ImageResizer.ResizeToCenter(fileStream, size),
+                                        size = size
+                                    }
+                            );
 
-                    //Generate thumbnails
-                    var thumbnails = new List<Image>();
-                    foreach (var size in Thumbnail.Sizes)
-                        thumbnails.Add(ImageResizer.ResizeToCenter(size, img));
+                        //Resize image itself
+                        var resizedImage = ImageResizer.Resize(
+                            fileStream,
+                            ImagesConfiguration.PhotoMaxWidth,
+                            ImagesConfiguration.PhotoMaxWidth
+                        );
 
-                    //Resize image itself
-                    var resizedImage = ImageResizer.Resize(img, ImagesConfiguration.PhotoMaxWidth, ImagesConfiguration.PhotoMaxWidth);
+                        // Generate image filename
+                        var fileName = MD5.Create().CalculateStringHash(
+                                DateTime.Now.Ticks.ToString()
+                            )
+                            .ToBase64String() + ".jpg";
 
-                    var fileName = MD5.Create().CalculateStringHash(DateTime.Now.Ticks.ToString()).ToBase64String() + ".jpg";
-
-                    //Zastąp istniejące zdjęcie głowne, jeżeli było ono już ustawione
-                    if (isMain)
-                    {
-                        var currentMainPhoto = _dbContext.Photos.Where(p => p.Type == PhotoType.Main && p.PlaceId == placeId).FirstOrDefault();
-                        if (currentMainPhoto != null)
+                        // Replace main photo if it was already assigned
+                        if (isMain)
                         {
-                            //Usuń thumbnail i wlasciwe zdjecie
-                            System.IO.File.Delete(ImagesConfiguration.PhotosPath + "\\" + currentMainPhoto.Filename);
-                            deleteAllThumbnails(currentMainPhoto.Filename);
-                            
-                            _dbContext.Photos.Remove(currentMainPhoto);
+                            var currentMainPhoto = dbContext.Photos
+                                .Where(p => p.Type == PhotoType.Main && p.PlaceId == placeId)
+                                .FirstOrDefault();
+                            if (currentMainPhoto != null)
+                            {
+                                // Delete thumbnails and the photo itself
+                                var filePath = Path.Combine(
+                                    ImagesConfiguration.PhotosPath,
+                                    currentMainPhoto.Filename
+                                );
+                                System.IO.File.Delete(filePath);
+                                deleteAllThumbnails(currentMainPhoto.Filename);
+                                
+                                dbContext.Photos.Remove(currentMainPhoto);
+                            }
                         }
-                    }
-
-                    try
-                    {
-                        //Save thumbnails
-                        foreach (var thumbnail in thumbnails)
+                        // Save thumbnails
+                        try
                         {
-                            var path = Thumbnail.Path(thumbnail.Size, fileName);
-                            //Create directory if not existing
-                            (new FileInfo(path)).Directory.Create();
-                            //save each thumbnail
-                            thumbnail.Save(path, ImageFormat.Jpeg);
+                            foreach (var thumbnail in thumbnails)
+                            {
+                                var path = Thumbnail.Path(thumbnail.size, fileName);
+                                // Create directory if not existing
+                                (new FileInfo(path)).Directory.Create();
+                                // Save each thumbnail
+                                var thumbnailFile = System.IO.File.Create(path);
+                                using (var thumbnailWriter = new System.IO.StreamWriter(thumbnailFile)) {
+                                    await thumbnail.stream.CopyToAsync(thumbnailWriter.BaseStream);
+                                }
+                            }
+
+                            //Save image
+                            var photoFile = System.IO.File.Create(fileName);
+                            using (var photoWriter = new System.IO.StreamWriter(photoFile)) {
+                                await resizedImage.CopyToAsync(photoWriter.BaseStream);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            return Json(new ResultViewModel() { Status = (int)ResultStatus.Error, Message = "Niewłaściwy plik obrazu. Spróbuj jeszcze raz." });
                         }
 
-                        //Save image
-                        resizedImage.Save(ImagesConfiguration.PhotosPath + "\\" + fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
-                    }
-                    catch (Exception)
-                    {
-                        return Json(new ResultViewModel() { Status = (int)ResultStatus.Error, Message = "Niewłaściwy plik obrazu. Spróbuj jeszcze raz." });
-                    }
+                        dbContext.Photos.Add(new Photo()
+                        {
+                            Filename = fileName,
+                            PlaceId = placeId,
+                            Type = isMain ? PhotoType.Main : PhotoType.Additional
+                        });
 
-                    _dbContext.Photos.Add(new Photo()
-                    {
-                        Filename = fileName,
-                        PlaceId = placeId,
-                        Type = isMain ? PhotoType.Main : PhotoType.Additional
-                    });
-
-                    _dbContext.SaveChanges();
+                        dbContext.SaveChanges();
+                    }
                 }
             }
             return Json(new ResultViewModel() { Status = (int)ResultStatus.Success });
@@ -117,12 +160,16 @@ namespace Vacation24.Controllers
         [Authorize(Roles = "owner, admin")]
         public ActionResult Delete(int photoId)
         {
-            var photo = _dbContext.Photos.Find(photoId);
+            var photo = dbContext.Photos.Find(photoId);
 
             //Check for access permission
-            if (!IsCurrentUserAdmin() && !
-                _dbContext.Places.Any(p => p.Id == photo.PlaceId && 
-                                      p.OwnerId == WebSecurity.CurrentUserId))
+            if (
+                !IsCurrentUserAdmin() && !
+                dbContext.Places.Any(
+                    p => p.Id == photo.PlaceId && 
+                    p.OwnerId == currentUserProvider.UserId
+                )
+            )
             {
                 return Json(new ResultViewModel()
                 {
@@ -132,13 +179,17 @@ namespace Vacation24.Controllers
             }
 
             //Delete thumbnail and actual photo
+            var filePath = Path.Combine(
+                ImagesConfiguration.PhotosPath,
+                photo.Filename
+            );
             deleteAllThumbnails(photo.Filename);
-            System.IO.File.Delete(ImagesConfiguration.PhotosPath + "\\" + photo.Filename);
+            System.IO.File.Delete(filePath);
             
-            _dbContext.Photos.Remove(photo);
-            _dbContext.SaveChanges();
+            dbContext.Photos.Remove(photo);
+            dbContext.SaveChanges();
 
-            return Json(new ResultViewModel() { Status = (int)ResultStatus.Success});
+            return Json(new ResultViewModel() { Status = (int)ResultStatus.Success });
         }
 
         private void deleteAllThumbnails(string fileName)
